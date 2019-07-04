169c0139e7de413a85567bb9fa5821f0050cdb1a
Jonnie Dredge <mrmightypants@gmail.com>
2019-07-04 13:33:19 -0400

    basic export to text file

---
+++ app/helpers/note_set_helper.rb
@@ -0,0 +1,2 @@
+module NoteSetHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
 
+  resources :note_set
+
+  post '/note_set/export', to: 'note_set#export', as: 'export'
+
   root 'home#index'
 end
