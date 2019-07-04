ad7ea363d1ee7020fcb5705142afa9709e4725d5
Jonnie Dredge <mrmightypants@gmail.com>
2019-07-04 14:39:14 -0400

    added basic unit tests

--- app/controllers/note_set_controller.rb
+++ app/controllers/note_set_controller.rb
@@ -1,10 +1,13 @@
 class NoteSetController < ApplicationController
 
     def export
-       p = params[:note_set] 
-       send_data p
+       p = params[:note_set]
+       set = NoteSet.new(article_params)
+       send_data set.text
     end
 
-    def show
+  private
+    def article_params
+        params.require(:note_set).permit(:text)
     end
 end
