7313bdb27e719b97e23017290cc91b3be738130f
Jonnie Dredge <mrmightypants@gmail.com>
2019-07-07 16:11:43 -0400

    updated tests, test fixtures for user

--- app/controllers/note_set_controller.rb
+++ app/controllers/note_set_controller.rb
@@ -1,13 +1,13 @@
 class NoteSetController < ApplicationController
 
     def export
-       p = params[:note_set]
-       set = NoteSet.new(article_params)
-       send_data set.text
+      p = params[:note_set]
+      set = NoteSet.new(note_set_params)
+      send_data set.text
     end
 
   private
-    def article_params
+    def note_set_params
         params.require(:note_set).permit(:text)
     end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
+  def setup
+    @user = User.find(980190962)
+  end
   
   test "create user" do
-    u = User.new(email: "test@test.ttttt", password_digest: "dhdhd")
-    assert true
+    assert_equal "test@test.test", @user.email
   end
+
+  test "update email" do
+    @user.email = "changed@test.test"
+    assert @user.save
+    @user.reload
+    assert_equal "changed@test.test", @user.email
+  end
+
+  #test "get user note sets" do
+
 end
