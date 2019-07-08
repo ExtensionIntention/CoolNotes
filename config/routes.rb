d608a3092b5b29a34b3b1db743e79170b009c6a6
Jonnie Dredge <mrmightypants@gmail.com>
2019-07-08 14:18:28 -0400

    cleaning up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
+
+  resources :infos
+  resources :users
+  resources :note_set
+  post '/note_set/export', to: 'note_set#export', as: 'export'
+
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  get 'home/index'
 
-  resources :note_set
-
-  post '/note_set/export', to: 'note_set#export', as: 'export'
-
-  resources :users
   resources :sessions, only: [:new, :create, :destroy]
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
+  get 'home/index'
   root 'home#index'
 end
