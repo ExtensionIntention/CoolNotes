96169aa17c7ee36999272aecf7f5c52c96ec2b53
Jonnie Dredge <mrmightypants@gmail.com>
2019-07-08 14:43:39 -0400

    moved login/signup links to menu, removed cache files from repo

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_07_07_181635) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-
 
   create_table "infos", force: :cascade do |t|
     t.string "Name"
