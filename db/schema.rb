ccbf7f18f34bc941bc4de6a0a953daccc9455df5
Jonnie Dredge <mrmightypants@gmail.com>
2019-07-10 22:14:42 -0400

    added tests

--- db/schema.rb
+++ db/schema.rb
@@ -15,17 +15,6 @@ ActiveRecord::Schema.define(version: 2019_07_10_183254) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "ckeditor_assets", force: :cascade do |t|
-    t.string "data_file_name", null: false
-    t.string "data_content_type"
-    t.integer "data_file_size"
-    t.string "data_fingerprint"
-    t.string "type", limit: 30
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["type"], name: "index_ckeditor_assets_on_type"
-  end
-
   create_table "infos", force: :cascade do |t|
     t.string "Name"
     t.string "Email"
